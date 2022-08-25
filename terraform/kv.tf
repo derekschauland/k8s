527590fa90eebcf5084d579ca8a0030ba55be63b
Derek Schauland <derek@derekschauland.com>
2022-08-25 07:21:18 -0500

    add kv and sp with pwd

---
+++ terraform/kv.tf
@@ -0,0 +1,33 @@
+resource "azurerm_key_vault" "this" {
+  name                = "${module.naming.key_vault.name}-${var.location}-${var.environment}-${var.owner}"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+
+  access_policy {
+    object_id = data.azuread_client_config.current.object_id #"9ad75dee-6105-4ffd-9c88-8255904692c0"
+    tenant_id = data.azurerm_client_config.current.tenant_id
+
+    secret_permissions = [
+      "Get", "List", "Purge", "Delete", "Backup", "Recover", "Restore", "Set"
+    ]
+  }
+  access_policy {
+    object_id = "8c443125-3a52-47a1-9011-4a6a823cbd1a"
+    tenant_id = data.azurerm_client_config.current.tenant_id
+
+    secret_permissions = [
+      "Get", "List", "Purge", "Delete", "Backup", "Recover", "Restore", "Set"
+    ]
+  }
+
+}
+
+resource "azurerm_key_vault_secret" "this" {
+  name         = "kubeconfig"
+  key_vault_id = azurerm_key_vault.this.id
+  value        = azurerm_kubernetes_cluster.this.kube_config_raw
+
+}
---
+++ terraform/svc_acct.tf
@@ -0,0 +1,33 @@
+resource "azuread_application" "app" {
+  display_name = "test-app"
+  owners = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal" "sp" {
+  application_id = azuread_application.app.application_id
+  app_role_assignment_required = false
+  owners = [data.azuread_client_config.current.object_id]
+}
+
+resource "time_rotating" "spin" {
+  rotation_days = 1
+}
+
+
+resource "azuread_service_principal_password" "sp" {
+  service_principal_id = azuread_application.app.id
+  rotate_when_changed = {
+    "rotation" = time_rotating.spin.id
+  }
+}
+
+resource "azurerm_key_vault_secret" "appid" {
+  name = "${azuread_application.app.display_name}-app-id"
+  value = azuread_application.app.application_id
+  key_vault_id = azurerm_key_vault.this.id
+}
+
+resource "azurerm_key_vault_secret" "pass" {
+  name = "${azuread_application.app.display_name}-app-secret"
+  value = azuread_service_principal_password.sp.value
+}
